32801429f1093dc93de51866f8d24f0018d8c1c9
Ethan Ho <hoea2@uci.edu>
2024-01-18 18:28:30 -0800

    added single invite links to slash cmds!

---
+++ commands/utility/invite.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder, Invite } = require("discord.js");
+require('dotenv').config();
+const cs161channels = process.env.CS161.split(' ');
+const ics45jchannels = process.env.ICS45J.split(' ');
+
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("invite")
+    .setDescription("generate single use invite"),
+
+    async execute(interaction) {
+      const user_channel = interaction.channelId;
+      const channel = cs161channels.includes(user_channel) ? process.env.CS161_INVITE : ics45jchannels.includes(user_channel) ? process.env.ICS45J_INVITE : null;
+      const invite = await interaction.guild.invites.create(channel, { maxAge: 86400, maxUses: 1});
+      await interaction.reply(`Here is your single use invite link: ${invite.url}`);
+    }
+}
