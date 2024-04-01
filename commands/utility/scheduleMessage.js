7cffcb1f061234d7f25058f6bf3c12497a028857
Ethan Ho <hoea2@uci.edu>
2024-04-01 12:00:05 -0700

    swap to different ping channel

--- commands/utility/scheduleMessage.js
+++ commands/utility/scheduleMessage.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
 require('dotenv').config();
 const schedule = require('node-schedule');
-// const cs161 = process.env.CS161_ANNOUNCEMENTS;
-// const ics45j = process.env.ICS45J_ANNOUNCEMENTS;
-const cs161 = '1106385921986789426';
-const ics45j = '1169744325928833057';
+const cs161 = process.env.CS161_ANNOUNCEMENTS;
+const ics45j = process.env.ICS45J_ANNOUNCEMENTS;
+// const cs161 = '1106385921986789426';
+// const ics45j = '1169744325928833057';
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,33 +19,11 @@ module.exports = {
         const message = interaction.options.getString("message");
         const datestring = interaction.options.getString("datestring");
         const time = interaction.options.getString("time");
-        // const channel = interaction.client.channels.cache.get('1106385921986789426');
         const channel = interaction.channelId == process.env.CS161_ANNOUNCEMENTS ? interaction.client.channels.cache.get(cs161) : interaction.channelId == process.env.ICS45J_ANNOUNCEMENTS ? interaction.client.channels.cache.get(ics45j) : null;
-        // const channel = interaction.channelId;
         const ping_role = interaction.channelId == process.env.CS161_ANNOUNCEMENTS ? process.env.CS161_ROLE : interaction.channelId == process.env.ICS45J_ANNOUNCEMENTS ? process.env.ICS45J_ROLE : null;
         const datetime = new Date(`${datestring}T${time}:00`);
         console.log(datetime, channel, ping_role);
-        // schedule.scheduleJob(datetime, ((chan, msg, role) => {chan.send(`<> ${msg}`);}).bind(null, channel, message, ping_role));
         schedule.scheduleJob(datetime, ((chan, msg, role) => {chan.send(`<@&${role}> ${msg}`);}).bind(null, channel, message, ping_role));
         await interaction.reply({ephemeral: true, content: `Scheduled ${message} for ${datetime}`});
     }
 }
-
-
-
-// module.exports = {
-//     data: new SlashCommandBuilder()
-//         .setName("reminder")
-//         .setDescription("schedule reminders")
-        // .addStringOption(option => option.setName("datestring").setRequired(true))
-        // .addStringOption(option => option.setName("message").setRequired(true)),
-
-//         async execute(interaction) {
-            // const datestring = interaction.option.getString("datestring");
-            // const message = interaction.option.getString("message");
-            // // const channel = interaction.channelID == cs161 ? cs161 : interaction.channelID == ics45j ? ics45j : null;
-            // // schedule.scheduleJob();
-            // await interaction.reply({ephemeral: true, content: `Scheduled ${message} for ${datestring}`});
-//         }
-
-// }
